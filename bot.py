fe4e645ccb4ed4f3f53157ba5c15afa729fc57d7
akhudiak <artem.khudiak@gmail.com>
2024-11-30 21:45:13 +0200

    Removed unnecessary errors

--- bot.py
+++ bot.py
@@ -8,12 +8,6 @@ def input_error(func):
     def inner(args, book):
         try:
             return func(args, book)
-        except ValueError:
-            return "Give me name and phone please"
-        except IndexError:
-            return "Enter user name"
-        except KeyError:
-            return "Contact is not exists"
         except (PhoneFormatError, BirthdayFormatError, InputFormatError, NameError, PhoneError) as error:
             return error
     
